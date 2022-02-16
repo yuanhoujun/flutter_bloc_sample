ed7807ab1bd3a937d45e68bd3157580f6c5d7977
欧阳锋 <ouyangfeng2016@gmail.com>
2022-02-16 22:57:23 +0800

    修正部分代码

--- lib/counter/cubit/counter_cubit.dart
+++ lib/counter/cubit/counter_cubit.dart
@@ -4,7 +4,6 @@ class CounterCubit extends Cubit<int> {
 
   CounterCubit(int initialState) : super(initialState);
 
-
   void increment() => emit(state + 1);
 
   void decrement() => emit(state - 1);
